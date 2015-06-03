bc9d3c85cdeb788cb4ba6ee578fcf105e93b0d45
Aangelinaa <aewedemeyer@yahoo.com>
2015-06-03 11:51:48 -0400

    add call for next images when user reaches bottom of page

--- js/instants.js
+++ js/instants.js
@@ -1,29 +1,38 @@
-$.ajax({
+
+
+	var //outerPane = $("div").find(".detailsPaneOuter"),
+	   	didScroll = false
+	   	nextImagesUrl = "";
+
+    $(window).scroll(function(){
+    	didScroll = true;
+    });
+
+    loadImages("https://api.instagram.com/v1/users/48121533/media/recent/?access_token=48121533.5db54f4.df09ebad5d3d47d3979743c9c3b57b37");
+
+	function loadImages(thisUrl){
+		$.ajax({
 	      type: "GET",
 	      dataType: "jsonp",
 	      cache: false,
-	      url: "https://api.instagram.com/v1/users/48121533/media/recent/?access_token=48121533.5db54f4.df09ebad5d3d47d3979743c9c3b57b37",
+	      url: thisUrl,	      
 	      success: function(data) {
-	        for (var i = 0; i < 7; i++) {
+	        for (var i = 0; i < data.data.length; i++) {
 	        $(".latest").append("<li><a target='_blank' href='" + data.data[i].link +"'><img   src='" + data.data[i].images.standard_resolution.url +"'></img></a></li>");
 	        }
+	        nextImagesUrl = data.pagination.next_url;
 	      }
-	    });
+	    });   
+	    }; 
 
-	    var //outerPane = $("div").find(".detailsPaneOuter"),
-	    	didScroll = false;
-
-	    $(window).scroll(function(){
-	    	didScroll = true;
-	    });
-
-	    setInterval(function(){
-	    	if(didScroll){
-	    		//http://stackoverflow.com/questions/5036850/how-to-detect-page-scroll-to-a-certain-point-in-jquery
-	    		if($(window).scrollTop() == $(document).height()-$(window).height()){
-		    	//if(outerPane){
-		    		didScroll = false;
-		    		console.log("Load in more results");
-		    	}
+    setInterval(function(){
+    	if(didScroll){
+    		//http://stackoverflow.com/questions/5036850/how-to-detect-page-scroll-to-a-certain-point-in-jquery
+    		if($(window).scrollTop() == $(document).height()-$(window).height()){
+	    	//if(outerPane){
+	    		didScroll = false;
+	    		//console.log("Load in more results");
+	    		loadImages(nextImagesUrl);
 	    	}
-	    }, 250);
+    	}
+    }, 250);
